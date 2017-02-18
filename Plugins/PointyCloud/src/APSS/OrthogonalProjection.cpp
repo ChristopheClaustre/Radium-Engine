6bb47243358e2e7eb22469ee95847146e523af78
ChristopheClaustre <christophe.claustre@gmail.com>
2017-02-18 07:24:20 +0100

    ajout de l'éligibilité

--- Plugins/PointyCloud/src/APSS/OrthogonalProjection.cpp
+++ Plugins/PointyCloud/src/APSS/OrthogonalProjection.cpp
@@ -24,27 +24,31 @@ void OrthogonalProjection::project(PointyCloud &upSampledCloud)
 
     for(auto &p : upSampledCloud.m_points)
     {
-        fit.init(p.pos());
-
-        std::vector<int> neighbors = m_selector->getNeighbors(p);
-
-        int i = 0;
-        int res;
-        do
+        if (p.isEligible())
         {
-            for(auto &idx : neighbors)
-                fit.addNeighbor(m_originalCloud->m_points[idx]);
+            fit.init(p.pos());
 
-            res = fit.finalize();
-//            std::cout << "finalize() -> " << res << std::endl << std::flush;
-            i++;
-        } while(res!=Grenaille::STABLE/*Grenaille::NEED_OTHER_PASS*/ && i<MAX_FITTING_ITERATION);
+            std::vector<int> neighbors = m_selector->getNeighbors(p);
 
-//        std::cout << "nombre de fitting : " << i << std::endl << std::flush;
-//        std::cout << "avant : " << p.pos().transpose() << " après : " << fit.project(p.pos()).transpose() << std::endl << std::flush;
+            int i = 0;
+            int res;
+            do
+            {
+                for(auto &idx : neighbors)
+                    fit.addNeighbor(m_originalCloud->m_points[idx]);
 
-        APoint _p(fit.project(p.pos()), fit.primitiveGradient(p.pos()), p.color());
-        p = _p;
+                res = fit.finalize();
+//                std::cout << "finalize() -> " << res << std::endl << std::flush;
+                i++;
+            } while(res == Grenaille::NEED_OTHER_PASS && i<MAX_FITTING_ITERATION);
+
+//            std::cout << "nombre de fitting : " << i << std::endl << std::flush;
+//            std::cout << "avant : " << p.pos().transpose() << " après : " << fit.project(p.pos()).transpose() << std::endl << std::flush;
+
+            auto newPos = fit.project(p.pos());
+            APoint _p(newPos, fit.primitiveGradient(newPos), p.color());
+            p = _p;
+        }
     }
 }
 
