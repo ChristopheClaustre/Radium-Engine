f17d4884e9b98881eaa448fe023d6194c3714b04
Thibault <thibault.le@live.fr>
2017-02-18 16:11:36 +0100

    first test for neighbors selection passed !

--- Plugins/PointyCloud/tests/tests/NeighborsSelectionTest.hpp
+++ Plugins/PointyCloud/tests/tests/NeighborsSelectionTest.hpp
@@ -7,24 +7,60 @@
 #include <APSS/NeighborsSelection.hpp>
 #include <APSS/NeighborsSelectionWithRegularGrid.hpp>
 
+#include <cstdlib>
 
 using namespace PointyCloudPlugin;
 
 namespace PointyCloudTests
 {
+
+    inline float random(float a, float b) {
+        return (float)std::rand()/RAND_MAX * (b-a) + a;
+    }
+
     class NeighborsSelectionTest : public Test
     {
         void run() override
         {
+            // points in [0,1]x[0,1]x[0,1]
             std::shared_ptr<PointyCloud> cloud = PointyCloudFactory::makeDenseCube(11, 0.1);
 
-            NeighborsSelection selectorRef(cloud, 0.15);
-            NeighborsSelectionWithRegularGrid selectorTest(cloud, 0.15);
+            NeighborsSelection selectorRef(cloud, 0.0);
+            NeighborsSelectionWithRegularGrid selectorTest(cloud, 0.0);
 
-            std::vector<int> resRef = selectorRef.getNeighbors(cloud->m_points.at(0).pos());
-            std::vector<int> resTest = selectorTest.getNeighbors(cloud->m_points.at(0).pos());
+            // number of selection test
+            const int testCount = 25;
 
-            RA_UNIT_TEST(resRef.size()==resTest.size(), "Wrong neigbors count");
+            std::srand(std::time(0));
+            Ra::Core::Vector3 clamp3;
+            Ra::Core::Vector4 clamp4;
+            clamp3.fill(1.0);
+            clamp4.fill(1.0);
+
+            for(int itest = 0; itest<testCount; itest++)
+            {
+                // random radius
+                float r = random(0.1, 1.0);
+                selectorRef.setInfluenceRadius(r);
+                selectorTest.setInfluenceRadius(r);
+
+                // random point
+                Ra::Core::Vector3 pos = 0.5*(Ra::Core::Vector3::Random()+clamp3);
+                Ra::Core::Vector3 nor = Ra::Core::Vector3::Random().normalized();
+                Ra::Core::Vector4 col = 0.5*(Ra::Core::Vector4::Random()+clamp4);
+
+                APoint p(pos, nor, col);
+
+                std::vector<int> resRef  = selectorRef.getNeighbors(p);
+                std::vector<int> resTest = selectorTest.getNeighbors(p);
+
+                RA_UNIT_TEST(resRef.size()==resTest.size(), "Wrong neigbors count");
+
+                for(auto& idxRef : resRef)
+                {
+                    RA_UNIT_TEST(std::find(resTest.begin(), resTest.end(), idxRef)!=resTest.end(), "Wrong neighbors");
+                }
+            }
         }
     };
     RA_TEST_CLASS(NeighborsSelectionTest);
