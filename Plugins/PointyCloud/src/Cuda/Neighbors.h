16f1929649de593525537387fd5ae30f957ae824
thibault <thibault.le@live.fr>
2017-02-26 00:46:58 +0100

    set-up neighbors selection on device

--- Plugins/PointyCloud/src/Cuda/Neighbors.h
+++ Plugins/PointyCloud/src/Cuda/Neighbors.h
@@ -8,9 +8,32 @@ namespace PointyCloudPlugin {
 namespace Cuda {
 
 template<typename F> __device__
-void processNeighbors(const Vector3& p, Scalar r, const RegularGrid& grid, F& fun)
+void processNeighbors(const Vector3& p, Scalar r, const RegularGrid& grid, const Vector3* positions, F& fun)
 {
-    //TODO apply fun to neighbors
+    // point in local coordinates
+    Vector3 q = p - grid.m_aabb.min();
+
+    // searching limits of a cube centered at q of length r
+    int imin = std::max((int)std::floor((q[0]-r)/grid.m_dx), 0);
+    int jmin = std::max((int)std::floor((q[1]-r)/grid.m_dy), 0);
+    int kmin = std::max((int)std::floor((q[2]-r)/grid.m_dz), 0);
+    int imax = std::min((int)std::floor((q[0]+r)/grid.m_dx), grid.m_nx-1);
+    int jmax = std::min((int)std::floor((q[1]+r)/grid.m_dy), grid.m_ny-1);
+    int kmax = std::min((int)std::floor((q[2]+r)/grid.m_dz), grid.m_nz-1);
+
+    // search
+    for(int k = kmin; k<=kmax; ++k)
+        for(int j = jmin; j<=jmax; ++j)
+            for(int i = imin; i<=imax; ++i)
+            {
+                int idxCell = grid.rawIndex(i, j, k);
+                int begin = grid.m_cells[idxCell].begin;
+                int length = grid.m_cells[idxCell].length;
+
+                for(int idx = begin; idx<begin+length; ++idx)
+                    if((p - positions[grid.m_indices[idx]]).norm() <= r)
+                        f(positions[grid.m_indices[idx]]);
+            }
 }
 
 } // namespace Cuda
