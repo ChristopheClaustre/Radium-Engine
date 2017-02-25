60392c42c8bed2b51b30244b57d3923dc6164d51
thibault <thibault.le@live.fr>
2017-02-25 20:41:28 +0100

    upsampling kernel set-up but not yet implemented

--- Plugins/PointyCloud/src/Cuda/UpsamplingKernel.h
+++ Plugins/PointyCloud/src/Cuda/UpsamplingKernel.h
@@ -7,16 +7,44 @@ namespace PointyCloudPlugin {
 namespace Cuda {
 
 __global__
-void computeSampleCountFixed(size_t sizeSelected, int m, int* splatCount)
+void computeSampleCountFixed(int sizeSelected, int m, int* splatCount)
 {
     for (int i = 0; i < sizeSelected; ++i)
         splatCount[i] = m;
 }
 
-__global__
-void generateSample(/*TODO*/)
-{
 
+__device__
+void genSplat(int idxBegin, int splatCount, const Vector3& positionIn, const Vector3& normalIn, const Vector4& colorIn,
+                                                  Vector3* positionOut, Vector3* normalOut, Vector4* colorOut)
+{
+    //TODO: Compute orthonormal basis U,V from normalIn
+    const Vector3 U;
+    const Vector3 V;
+
+    for (int k = 0; k < splatCount; ++k)
+    {
+        //TODO: compute generated splat position
+        /* /!\ k might be replace by i,j indices /?\ (cf upsampler)*/
+        // for now the splats are at the same position
+        positionOut[idxBegin+k] = positionIn;
+        normalOut[idxBegin+k] = normalIn;
+        colorOut[idxBegin+k] = colorIn;
+    }
+}
+
+
+__global__
+void generateSample(int sizeSelected, int* selected, int* splatCount, int* splatCountSum,
+                    Vector3* positionOriginal, Vector3* normalOriginal, Vector4* colorOriginal,
+                    Vector3* positionFinal,     Vector3* normalFinal,    Vector4* colorFinal)
+{
+    for (int i = 0; i < sizeSelected; ++i)
+    {
+        int idx = selected[i];
+        genSplat(splatCountSum[i], splatCount[i], positionOriginal[idx], normalOriginal[idx], colorOriginal[idx],
+                 positionFinal, normalFinal, colorFinal);
+    }
 }
 
 } // namespace Cuda
