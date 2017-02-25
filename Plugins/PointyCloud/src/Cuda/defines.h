8e7e0392ef830b3dc58b62c695a3bdcfc56a7daa
thibault <thibault.le@live.fr>
2017-02-25 14:25:28 +0100

    clean-up defines.h

--- Plugins/PointyCloud/src/Cuda/defines.h
+++ Plugins/PointyCloud/src/Cuda/defines.h
@@ -16,44 +16,12 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
    }
 }
 
-
-
-
 namespace PointyCloudPlugin {
 namespace Cuda {
 
     typedef float Scalar;
     typedef Eigen::Matrix<Scalar, 3, 1> Vector3;
     typedef Eigen::Matrix<Scalar, 4, 1> Vector4;
-
-
-
-
-//    // device vector to pass in kernel
-//    template<typename T>
-//    struct KernelArray
-//    {
-//        MULTIARCH
-//        KernelArray(thrust::device_vector<T>& dVec) :
-//            _array(thrust::raw_pointer_cast(&dVec[0])), _size(dVec.size()) {}
-
-//        MULTIARCH
-//        ~KernelArray(){}
-
-//        MULTIARCH
-//        T& operator [](size_t idx) {return _array[idx];}
-
-//        MULTIARCH
-//        const T& operator [](size_t idx) const {return _array[idx];}
-
-//        MULTIARCH
-//        const size_t& size() const {return _size;}
-
-//    private:
-//        T*      _array;
-//        size_t  _size;
-
-//    }; // struct KernelArray
 
 } // namespace Cuda
 } // namespace PointyCloudPlugin
